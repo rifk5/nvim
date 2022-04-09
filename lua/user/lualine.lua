16036564b94d19fe3e91456cecfe18096ce964c6
rifk5 <delarosa.git@gmail.com>
2022-04-09 19:16:31 -0400

    Changed lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -63,53 +63,23 @@ local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
-lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "codedark",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-		afileformatlways_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { mode },
-		lualine_b = { branch, diagnostics },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, "fileformat", "encoding", filetype },
-		lualine_y = {},
-		lualine_z = { location },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
-})
-
 -- lualine.setup({
 -- 	options = {
 -- 		icons_enabled = true,
--- 		theme = "auto",
+-- 		theme = "codedark",
 -- 		component_separators = { left = "", right = "" },
 -- 		section_separators = { left = "", right = "" },
 -- 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
--- 		always_divide_middle = true,
+-- 		afileformatlways_divide_middle = true,
 -- 	},
 -- 	sections = {
--- 		lualine_a = { branch, diagnostics },
--- 		lualine_b = { mode },
+-- 		lualine_a = { mode },
+-- 		lualine_b = { branch, diagnostics },
 -- 		lualine_c = {},
--- 		lualine_x = { "encoding", "fileformat", "filetype" },
--- 		-- lualine_x = { diff, spaces, "encoding", filetype },
--- 		lualine_y = { location },
--- 		lualine_z = { progress },
+-- 		-- lualine_x = { "encoding", "fileformat", "filetype" },
+-- 		lualine_x = { diff, "fileformat", "encoding", filetype },
+-- 		lualine_y = {},
+-- 		lualine_z = { location },
 -- 	},
 -- 	inactive_sections = {
 -- 		lualine_a = {},
@@ -122,3 +92,33 @@ lualine.setup({
 -- 	tabline = {},
 -- 	extensions = {},
 -- })
+
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { branch, diagnostics },
+		lualine_b = { mode },
+		lualine_c = {},
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		-- lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_y = { location },
+		lualine_z = { progress },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
